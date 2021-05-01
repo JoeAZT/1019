797990287675e2321b09ce2a171fa9f5205fdc75
Joe <joetaylor509@yahoo.com>
2021-05-01 16:26:51 +0100

    Initial Commit

---
+++ selfCare/selfCareApp.swift
@@ -0,0 +1,17 @@
+//
+//  selfCareApp.swift
+//  selfCare
+//
+//  Created by Joseph Taylor on 01/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct selfCareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
